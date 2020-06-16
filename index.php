f9f2ad1ef437174020cbabb274f168b668761356
Fabio Monzani <renderbros@gmail.com>
2020-06-16 01:55:02 +0100

    Preparing layout

--- index.php
+++ index.php
@@ -11,11 +11,268 @@
     <link rel="stylesheet" href="css/style.css">
     <!-- Lato font -->
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;900&display=swap" rel="stylesheet">
+    <!-- FontAwesome -->
+    <link href="css/fontawesome.min.css" rel="stylesheet">
+    <link href="css/duotone.min.css" rel="stylesheet">
+    <link href="css/brands.min.css" rel="stylesheet">
 
-    <title>Hello, world!</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-custom">
+      <a class="navbar-brand" href="#">
+        <img src="img/logo.svg" width="30" height="30" class="d-inline-block align-top mr-3" alt="" loading="lazy">
+        Cryptofonts
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#get-started">Get started</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Font</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Icons</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Github</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Donate</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="hero">
+      <img src="img/jumbotron.svg" width="100%">
+      <div class="hero-text">
+        <h1 class="mb-4">Beautifully crafted cryptocurrencies font and icons for your projects.</h1>
+        <p>1500+ Open-source CSS and SVG Icons</p>
+      </div>
+    </div>
+
+      <section>
+        <div class="container">
+          <h1 class="mb-4">Features</h1>
+          <div class="row">
+            <div class="col-md-6">
+              <p class="mb-5">Crypto Fonts gives you scalable vector icons that can instantly be customized by size, color, shadow, and anything that can be done with the power of CSS.</p>
+              <img class="img-fluid" src="img/bitcoin-vector.svg">
+            </div>
+            <div class="col-md-6">
+              <div class="row">
+              <div class="col-md-6">
+                <div class="card">
+                  <div class="card-body">
+                    <i class="fad fa-box-full"></i>
+                    <p>One font,1300+ icons</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="card">
+                  <div class="card-body">
+                    <i class="fad fa-feather"></i>
+                    <p>Lightweight</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="card">
+                  <div class="card-body">
+                    <i class="fab fa-js-square"></i>
+                    <p>No Javascript</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="card">
+                  <div class="card-body">
+                    <i class="fad fa-expand"></i>
+                    <p>Infinite scalability</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="card">
+                  <div class="card-body">
+                    <i class="fab fa-css3-alt"></i>
+                    <p>CSS controls</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="card">
+                  <div class="card-body">
+                    <i class="fad fa-eye"></i>
+                    <p>Retina ready</p>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+        <div class="row justify-content-center mt-5 mb-5">
+          <a href="#" class="btn btn-primary">Show me the icons</a>
+        </div>
+      </div>
+    </section>
+
+
+      <section>
+        <div class="container">
+          <h1 class="mb-4">Compatibility</h1>
+
+          <div class="row text-center">
+            <div class="col-md-2 col-6">
+              <img src="img/icon-sketch.svg" height="70px">
+              <p class="mt-3 mb-4">Sketch App</p>
+            </div>
+            <div class="col-md-2 col-6">
+              <img src="img/icon-photoshop.svg" height="70px">
+              <p class="mt-3 mb-4">Photoshop</p>
+            </div>
+            <div class="col-md-2 col-6">
+              <img src="img/icon-illustrator.svg" height="70px">
+              <p class="mt-3 mb-4">Illustrator</p>
+            </div>
+            <div class="col-md-2 col-6">
+              <img src="img/icon-xd.svg" height="70px">
+              <p class="mt-3 mb-4">Adobe XD</p>
+            </div>
+            <div class="col-md-2 col-6">
+              <img src="img/icon-figma.svg" height="70px">
+              <p class="mt-3 mb-4">Figma</p>
+            </div>
+            <div class="col-md-2 col-6">
+              <img src="img/icon-invisio.svg" height="70px">
+              <p class="mt-3 mb-4">InVision Studio</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="get-started" id="get-started">
+        <div class="container">
+          <h1 class="mb-4">Get Started</h1>
+          <p>Grap a copy of the latest version of Crypto Fonts and use it alongside your source code.</p>
+          <div class="row mt-5">
+            <div class="col-md-6">
+              <h4>1. Move the CSS and web fonts into your project.</h4>
+              <p>Copy the entire  webfonts folder and the  cryptofont.css into your project’s static assets directory (or where ever you prefer to keep front end assets or vendor stuff).</p>
+            </div>
+            <div class="col-md-6 text-center">
+              <img src="img/icon-transfer.svg">
+            </div>
+          </div>
+
+          <div class="row mt-5">
+            <div class="col-md-6">
+              <h4>2. Reference the CSS</h4>
+              <p>Add a reference to the  cryptofont.css styling file into the <code>&lt;head&gt;</code> of each template or page that you want to use Crypto Font on.
+              Pay attention to the pathing of your project and where you moved the files to in the previous step.</p>
+            </div>
+            <div class="col-md-6">
+              <div class="card terminal">
+                <div class="card-header">
+                  <ul class="list-inline">
+                    <li class="dot"></li>
+                    <li class="dot"></li>
+                    <li class="dot"></li>
+                    <li class="white float-right">index.html</li>
+                  </ul>
+                </div>
+                <div class="card-body">
+                  <p>
+                    &lt;<span class="markup">head</span>&gt;<br>
+                    &lt;!--CryptoFont--&gt;<br>
+                       &lt;<span class="markup">link</span> <span class="ref">href=</span><span class="link">"cryptofont.css"</span><span class="ref">rel=</span><span class="link">"stylesheet"</span>&gt;<br>
+                    &lt;<span class="markup">/head</span>&gt;
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row mt-5">
+            <div class="col-md-6">
+              <h4>3. Place icons in your UI’s markup</h4>
+              <p>With the references complete, you can now start placing icons in your HTML’s <code>&lt;body&gt;</code>
+              We recommend using a consistent HTML element, like an <code>&lt;i&gt;</code> You can place Crypto Fonts icons just about anywhere using the CSS Prefix cf and the crypto ticker.</p>
+            </div>
+            <div class="col-md-6">
+              <div class="card terminal">
+                <div class="card-header">
+                  <ul class="list-inline">
+                    <li class="dot"></li>
+                    <li class="dot"></li>
+                    <li class="dot"></li>
+                    <li class="white float-right">index.html</li>
+                  </ul>
+                </div>
+                <div class="card-body">
+                  <p class="card-text">
+                    &lt;<span class="markup">body</span>&gt;<br>
+                    &lt;!--some icons--&gt;<br>
+                       &lt;<span class="markup">i</span> <span class="ref">class="cf cf-btc"&gt;</span><span class="link">&lt;/i&gt;</span><br>
+                       &lt;<span class="markup">i</span> <span class="ref">class="cf cf-eth"&gt;</span><span class="link">&lt;/i&gt;</span><br>
+                       &lt;<span class="markup">i</span> <span class="ref">class="cf cf-ltc"&gt;</span><span class="link">&lt;/i&gt;</span><br>
+                    </body>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <img class="img-fluid" src="img/section-footer.svg">
+
+      <footer>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-3">
+              <img class="opacity-50" src="img/logo.svg" height="60px">
+            </div>
+            <div class="col-md-3">
+              <ul class="footer-list">
+                <li>Social</li>
+                <li><a href="#">Twitter</a></li>
+              </ul>
+            </div>
+            <div class="col-md-3">
+              <ul class="footer-list">
+                <li>Legal</li>
+                <li><a href="#">License</a></li>
+              </ul>
+            </div>
+            <div class="col-md-3">
+              <ul class="footer-list">
+                <li>Developers</li>
+                <li><a href="#">Github</a></li>
+                <li><a href="#">Gitcoin</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+      <div class="row">
+        <div class="col text-center">
+          <p class="small opacity-50">Made with love around the world</p>
+        </div>
+      </div>
+
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
