cef6fa9d1ce3a1dc01abeb244f463aa352d684a7
Fabio Monzani <renderbros@gmail.com>
2020-06-20 16:09:17 +0100

    Donation modal update with lonks and qr codes

--- include/donate.php
+++ include/donate.php
@@ -20,20 +20,23 @@
           <li class="nav-item">
             <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Gitcoin</a>
           </li>
-          <li class="nav-item">
+          <!-- <li class="nav-item">
             <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false"><i class="cf cf-btc"></i> Bitcoin</a>
-          </li>
+          </li> -->
         </ul>
         <div class="tab-content text-center" id="myTabContent">
           <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-            <p>Even 1 DAI would make the difference</p>
+            <p class="mt-4">Even 1 DAI would make the difference</p>
+            <img class="qrcode" src="img/eth-address.svg" width="150">
+            <p><span class="badge badge-pill badge-light">0xFC72b32F2f7caFCa4d5d99c031f4B8F9579c18b6</span></p>
           </div>
           <div class="tab-pane fade text-center" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-            <p>Donate on Gitcoin by sponsoring my grant</p>
+            <p class="mt-4"><a href="https://gitcoin.co/grants/720/crypto-fonts">Donate on Gitcoin by sponsoring my grant</a></p>
+            <img src="img/gitcoin-logo.svg" height="150">
           </div>
-          <div class="tab-pane fade text-center" id="contact" role="tabpanel" aria-labelledby="contact-tab">
-            <p>I know transactions cost are high, but if you feel generous here's my Bitcoin address</p>
-          </div>
+          <!-- <div class="tab-pane fade text-center" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+            <p class="mt-4">I know transactions cost are high, but if you feel generous here's my Bitcoin address</p>
+          </div> -->
         </div>
       </div>
       <div class="modal-footer">
