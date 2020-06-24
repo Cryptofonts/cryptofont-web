26872ac850795580287a847d6648abc386222d28
Fabio Monzani <renderbros@gmail.com>
2020-06-24 15:58:29 +0100

    Footer links open to a new page

--- include/footer.php
+++ include/footer.php
@@ -7,20 +7,20 @@
       <div class="col-md-3 col-3">
         <ul class="footer-list">
           <li>Social</li>
-          <li><a href="https://twitter.com/cryptofonts?lang=en">Twitter</a></li>
+          <li><a href="https://twitter.com/cryptofonts?lang=en" target="_blank">Twitter</a></li>
         </ul>
       </div>
       <div class="col-md-3 col-3">
         <ul class="footer-list">
           <li>Legal</li>
-          <li><a href="https://www.gnu.org/licenses/gpl-3.0.en.html">License</a></li>
+          <li><a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="_blank">License</a></li>
         </ul>
       </div>
       <div class="col-md-3 col-3">
         <ul class="footer-list">
           <li>Developers</li>
-          <li><a href="https://github.com/monzanifabio/cryptofont">Github</a></li>
-          <li><a href="https://gitcoin.co/grants/720/crypto-fonts">Gitcoin</a></li>
+          <li><a href="https://github.com/monzanifabio/cryptofont" target="_blank">Github</a></li>
+          <li><a href="https://gitcoin.co/grants/720/crypto-fonts" target="_blank">Gitcoin</a></li>
         </ul>
       </div>
     </div>
