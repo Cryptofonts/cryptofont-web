6397df3caa08d06af5ffbdb5d7d3458a321283f8
FabioMonzani <renderbros@gmail.com>
2021-10-08 02:30:41 +0100

    Updated purgecss to include fontawesome brands

--- purgecss.config.js
+++ purgecss.config.js
@@ -6,6 +6,7 @@ module.exports = {
   css: [
     'vendors/bootstrap-4.5.0/css/bootstrap.min.css',
     'vendors/fontawesome/duotone.min.css',
-    'vendors/fontawesome/fontawesome.min.css'
+    'vendors/fontawesome/fontawesome.min.css',
+    'vendors/fontawesome/brands.min.css'
   ]
 }
