3a6ebd172395cb0105ed1d89199ac0f2c46bfb3c
FabioMonzani <renderbros@gmail.com>
2023-05-22 07:11:52 +0100

    Removed unused files

--- _bin/include/getcount.php
+++
@@ -1,9 +0,0 @@
-<?php
-//get database account details
-include('dbaccess.php');
-
-
-$con = mysqli_connect($db_hostname,$db_username,$db_password,$db_database);
-$count = mysqli_num_rows(mysqli_query($con, "SELECT * FROM coloricon"));
-
-?>
