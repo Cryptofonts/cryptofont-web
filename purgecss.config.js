297cb90dab126ccc1570f14a94eaab5864e6f6cf
monzanifabio <renderbros@gmail.com>
2023-08-16 17:44:00 +0100

    Fixed licensing page

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   content: ["*.html"],
-  css: ["vendors/bootstrap-5.3.1/css/bootstrap.min.css", "vendors/fontawesome/duotone.min.css", "vendors/fontawesome/fontawesome.min.css", "vendors/fontawesome/brands.min.css"],
+  css: ["vendors/bootstrap-5.3.1/css/bootstrap.min.css"],
 };
